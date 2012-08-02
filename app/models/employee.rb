6559578b2beebe56ab0cca940e6b0f7307b3aca4
Valeriy <valloskut@gmail.com>
2012-08-02 22:31:59 +0600

    added simple_form to display restaurant

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,5 +1,5 @@
 class Employee < ActiveRecord::Base
-  attr_accessible :name,:marital_status, :habits
+  attr_accessible :name,:marital_status, :habits, :restaurant_id
   store :other, accessors: [:marital_status, :habits]
   belongs_to :restaurant
 end
